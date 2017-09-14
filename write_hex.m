4de7c03fc6f7b5979cf561e38637bf7bbe62747a
Milton <miljo274@student.liu.se>
2017-09-14 21:53:36 +0200

    Done with untill 1.5

--- write_hex.m
+++ write_hex.m
@@ -5,8 +5,7 @@ function y = write_hex(filename,x)
   x(x<-32768) = -32768;
   x(x>32767) = 32767;
   x(x<0) = x(x<0)+65536;  % Handle two's complement representation
-
-  y = fprintf(fid,'%04x\n',x);
+  y = fprintf(fid,'.dw 0x%04x\n',x);
   fclose(fid);
 
 
